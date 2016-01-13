61055ba5ee7e3616b3e79ae994ca61a783a9a5ce
java-city <jc@pulpjava.com>
2016-01-13 08:58:01 -0500

    Added counters for wins, losses and ties

--- Game.java
+++ Game.java
@@ -1,47 +1,35 @@
 import javax.swing.*;
-/*
- * The code runs, but there is an error in logic in the code. What's the error?
- * Add five new variables:
- * 		wins
- * 		losses
- * 		ties
- * 		clientGesture
- * 		serverGesture
- * 
- * When the game completes, print out a summary, such as:
- * 
- * You picked rock.
- * Server picked rock.
- * This was a tie.
- * 
- * Here is the summary so far:
- * wins: 3
- * ties: 0
- * losses:5
- * 
- */
+
 public class Game {
 
 	public static void main(String args[]) {
 
-		String input = "";
+		int wins=0, losses=0, ties = 0;
+		String clientGesture = null;
+		String serverGesture = "rock";
+		
 		do {
 			String prompt = "Will it be rock, paper or scissors?";
-			input = JOptionPane.showInputDialog(prompt);
+			clientGesture = JOptionPane.showInputDialog(prompt);
+			//clientGesture = input;
 			String result = "error";
 
-			if (input.equalsIgnoreCase("rock")) {
+			if (clientGesture.equalsIgnoreCase("rock")) {
 				result = "tie";
+				ties++;
 			}
 
-			if (input.equalsIgnoreCase("scissors")) {
+			if (clientGesture.equalsIgnoreCase("scissors")) {
 				result = "lose";
+				losses = losses + 1;
 			}
 
-			if (input.equalsIgnoreCase("paper")) {
+			if (clientGesture.equalsIgnoreCase("paper")) {
 				result = "win";
+				wins++;
 			}
 			JOptionPane.showMessageDialog(null, result);
-		} while (!input.equalsIgnoreCase("q"));
+			System.out.println("\nWins: " + wins + "\tLosses: "+ losses + "\tTies: " + ties);
+		} while (!clientGesture.equalsIgnoreCase("q"));
 	}
 }
